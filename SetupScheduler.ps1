1519194900f1511613d6f01adc10cd2078bfb540
Dan <danielmassarano@gmail.com>
2020-07-12 16:33:29 +0100

    Updated setup script to elevate privileges

--- SetupScheduler.ps1
+++ SetupScheduler.ps1
@@ -5,9 +5,9 @@ $Argument = $location.Path + "\WindowsMaintenenceStep1.ps1"
 $Trigger = New-ScheduledTaskTrigger -Weekly -WeeksInterval $config.WeeksInterval -DaysOfWeek $config.DaysOfWeek -At $config.TimeToTRun
 $User = $env:UserDomain + '\' + $env:UserName
 $Action = New-ScheduledTaskAction -Execute "PowerShell.exe" -Argument $Argument
-Register-ScheduledTask -TaskName "WindowsMaintenenceStep1" -Trigger $Trigger -User $User -Action $Action
+Register-ScheduledTask -TaskName "WindowsMaintenenceStep1" -Trigger $Trigger -User $User -Action $Action -RunLevel Highest
 
 $Argument = $location.Path + "\WindowsMaintenenceStep2.ps1"
 $Trigger = New-ScheduledTaskTrigger -AtStartup
 $Action = New-ScheduledTaskAction -Execute "PowerShell.exe" -Argument $Argument
-Register-ScheduledTask -TaskName "WindowsMaintenenceStep2" -Trigger $Trigger -User $User -Action $Action
+Register-ScheduledTask -TaskName "WindowsMaintenenceStep2" -Trigger $Trigger -User $User -Action $Action -RunLevel Highest
